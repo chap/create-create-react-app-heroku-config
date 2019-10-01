7a32bed2b2ec757eaa546fd33766158ddbe518a6
Chap Ambrose <chapambrose@gmail.com>
2019-10-01 19:23:58 +0000

    add reference to REACT_APP_NOT_SECRET_CODE variable

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          REACT_APP_NOT_SECRET_CODE={process.env.REACT_APP_NOT_SECRET_CODE}
         </p>
         <a
           className="App-link"
